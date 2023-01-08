2600a5086f89d992e8e5257d55992d69c708a98f
ayanny <ayanny_maria@hotmail.com>
2023-01-08 15:55:23 -0300

    feat: configuracao banco h2 e criacao perfil teste

--- src/main/java/com/backend/workshop/entities/User.java
+++ src/main/java/com/backend/workshop/entities/User.java
@@ -2,6 +2,12 @@ package com.backend.workshop.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,11 +19,15 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
 private static final long serialVersionUID = 1L;
 	
 	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
